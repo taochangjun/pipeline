ce11538abe1cca4662808810d62f475156e51074
taochangjun <taochangjun@ymt360.com>
2021-06-23 17:13:14 +0800

    first pipeline

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/23 5:06 PM
+# @Author  : Thomas
+# @File    : main.py
+
+
+def test():
+    print("Hello pipeline")
+
+if __name__ == '__main__':
+    test()
